2ca3eb72f682737ce588fc772f9ab19f3cb10d00
rktrivedi <trivedi.rohit@outlook.com>
2024-02-06 19:43:11 +0530

    Server Connection Pending

--- server/index.js
+++ server/index.js
@@ -34,6 +34,8 @@ io.on("connection", (socket) => {
 
     if (error) return callback(error);
 
+    socket.join(user.room);
+
     socket.emit("message", {
       user: "admin",
       text: `${user.name},Welcome to the room ${user.room}`,
@@ -46,18 +48,28 @@ io.on("connection", (socket) => {
     callback();
     console.log(name, room);
 
-    // if (error) {
-    //   callback({ error: "error" });
-    // }
+    io.to(user.room).emit("rooData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
   });
 
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
 
     io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit("roomData", { room: user.room, text: message });
+    callback();
   });
 
   socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name}has left`,
+      });
+    }
     console.log("User had left !!!");
   });
 });
